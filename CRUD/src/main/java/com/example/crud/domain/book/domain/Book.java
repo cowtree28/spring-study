3d57edca894f1935bb434e26bfba0dc6c020f88f
treegus <cowtree28@gmail.com>
2026-07-04 18:00:52 +0900

    lombok 적용

--- CRUD/src/main/java/com/example/crud/domain/book/domain/Book.java
+++ CRUD/src/main/java/com/example/crud/domain/book/domain/Book.java
@@ -1,30 +1,18 @@
 package com.example.crud.domain.book.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Book {
-    public void Book() {}
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column
     private String title;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
